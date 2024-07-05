8d553f8f3c7d1a8064051db91428ee4c89ffc6b3
Sampson Chang <sampson.chang@gmail.com>
2024-07-05 00:13:49 -0700

    made a test for the codes

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,17 @@
 const needle = require('needle');
-const breed = process.argv[2];
-needle.get(`https://api.thasdecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  if (error) {
-    console.log("There's a erro! ",error);
-  }
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+  needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    if (error) {
+      return callback(error, null);
+    }
+    if (response.statusCode === 200 && body.length !== 0) {
+      return callback(null,body[0].description);
+    } else {
+      return callback('Breed not found', null);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
